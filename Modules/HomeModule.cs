8890c569ac0f1091f1b60059808f4fdd29d9e315
Dave Denlow <Davedenlow@gmail.com>
2016-12-07 06:14:23 -0800

    add connection to multiple contact pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,16 +13,22 @@ namespace NewAddressBook
           foreach(var contact in currentRolodex)
           return View["index.cshtml", ourRolodex];
         };
-        Get["/contact/new"] = _ => {
-          return View["/add_new_contact", ];
-        };
-        Get["/contact/new"] = _ => {
-          return View["/add_new_contact", ];
+        Get["/contact/"] = _ => {
+          return View["added_contact", ];
         };
         Post["/contact/delete"] = _ => {
-          return View["/delete_contacts.cshtml"]
+          return View["delete_contacts.cshtml"]
+        };
+        Get["/contact/see1contact"] = _ => {
+          return View["single_contact.cshtml", ];
         };
         Get["/contact/new"] = _ => {
-          return View["/add_new_contact", ];
+          return View["add_new_contact", ];
         };
+        Get["/contacts/{id}"] = parameters => {
+        Var Friend = Friend.Find(parameters.id);
+        return View["contact.cshtml", requestedContact];
+      
+      };
+
     }
